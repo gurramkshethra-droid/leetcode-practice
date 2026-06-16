861955e5292162bcfe175ff6632a062cb1427f7b
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-06-16 17:36:49 +0530

    Added problem17 solution

---
+++ Phno.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        t = {
+            "2":["a","b","c"],
+            "3":["d","e","f"],
+            "4":["g","h","i"],
+            "5":["j","k","l"],
+            "6":["m","n","o"],
+            "7":["p","q","r","s"],
+            "8":["t","u","v"],
+            "9":["w","x","y","z"]
+        }
+
+        ans = [""]
+        for d in digits:
+            temp = []
+
+            for prefix in ans:
+                for ch in t[d]:
+                    temp.append(prefix + ch)
+
+            ans = temp
+
+        return ans
+
+
+
+        
