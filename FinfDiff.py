d39179cfdb7bfbd461778da07a288d7cdcf740bb
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-09-02 11:46:29 +0530

    Added problem389 solution

---
+++ FinfDiff.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        ans=0
+        for ch in s+t:
+            ans^=ord(ch)
+        return chr(ans)
